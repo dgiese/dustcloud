08b614ee7aed25c11980a7cda688619094ceef1e
coderus <coderusinbox@gmail.com>
2018-01-03 23:31:48 +0300

    Automatic flasher script
    Switched to python3
    Using python-miio module
    Automatically discover and handshake available device
    Can be used to flash old devices with known token
    Automatically serving http server
    Drawing progress bar while downloading firmware
    Automatically exiting after downloading completed
    
    Former-commit-id: 0eb2ba92e48eaab6e25e062e77c32d5361326e19

--- xiaomi.vacuum.gen1/firmwarebuilder/flasher.py
+++ xiaomi.vacuum.gen1/firmwarebuilder/flasher.py
@@ -1,77 +1,185 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import re
-import os
-import subprocess
-import time
 import socket
 import hashlib
 import json
+import sys
+import argparse
+import http.server
+import socketserver
+import threading
+from time import sleep
+import miio
+
+
+class ThreadedHTTPServer(socketserver.ThreadingMixIn, http.server.HTTPServer):
+    daemon_threads = True
+
+
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+    '''
+    https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console/34325723#34325723
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    '''
+    percent = ('{0:.' + str(decimals) + 'f}').format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    # Clear Line on Complete
+    if iteration == total:
+        print('\r' + ' ' * (len(prefix) + length + len(suffix) + 11))
+
 
 def md5(fname):
     hash_md5 = hashlib.md5()
-    with open(fname, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
+    with open(fname, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b''):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
 
+
 def findIP():
-    return ((([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0])
-
-from prompt_toolkit import prompt
-print('Flasher for Xiaomi Vacuum')
-print('trying to find the vacuum')
+    return ((([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith('127.')] or [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ['no IP found'])[0])
 
 
+def discover_devices():
+    timeout = 5
+    seen_addrs = []  # type: List[str]
+    addr = '<broadcast>'
+    # magic, length 32
+    helobytes = bytes.fromhex('21310020ffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    s.settimeout(timeout)
+    s.sendto(helobytes, (addr, 54321))
+    while True:
+        try:
+            data, addr = s.recvfrom(1024)
+            if addr[0] not in seen_addrs:
+                seen_addrs.append(addr[0])
+        except socket.timeout:
+            break  # ignore timeouts on discover
+        except Exception as ex:
+            print('Error while reading discover results:', ex)
+            break
+    return seen_addrs
 
 
-process = subprocess.Popen(['mirobo','discover','--handshake','true'], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-time.sleep(1);
-process.terminate();
-output = process.stderr.read()
+def select_item(welcome_text, items):
+    print(welcome_text)
+    for i, item in enumerate(items):
+        print('{}. {}'.format(i+1, item))
+    try:
+        selected = input('Please select option by typing number (1-{}): '.format(len(items)))
+        result = items[int(selected)-1]
+        return result
+    except KeyboardInterrupt:
+        print('User requested to exit')
+        exit()
+    except ValueError:
+        print('Error! Please enter only one number')
+        exit()
+    except IndexError:
+        print('Error! Please enter one number between 1-{}'.format(len(items)))
+        exit()
 
 
-#output = "IP 192.168.8.1: default - token: b'FFFFFFFFFFFFFFFFFFFFFF'"
-try:
-    m = re.search("b'(.+?)'", output)
-    token = m.group(1)
-    m = re.search("IP (.+?)[\: ]", output)
-    ip = m.group(1)
-except:
-    print('Vacuum not found...Check connection')
+def main():
+    parser = argparse.ArgumentParser(description='Flasher for Xiaomi Vacuum.\nFor specific options check \'{} --help\''.format(sys.argv[0]))
+    parser.add_argument('-a', '--address', dest='address', type=str, help='IP address of vacuum cleaner')
+    parser.add_argument('-t', '--token', dest='token', type=str, help='Known token of vacuum')
+    parser.add_argument('-f', '--firmware', dest='firmware', type=str, help='Path to firmware file')
+
+    args, external = parser.parse_known_args()
+
+    print('Flasher for Xiaomi Vacuum')
+
+    ip_address = args.address
+    known_token = args.token
+    firmware = args.firmware
+
+    if not args.firmware:
+        print('You should specify firmware file name to install')
+        exit()
+
+    if not ip_address:
+        print('Address is not set. Trying to discover.')
+        seen_addrs = discover_devices()
+
+        if len(seen_addrs) == 0:
+            print('No devices discovered.')
+            exit()
+        elif len(seen_addrs) == 1:
+            ip_address = seen_addrs[0]
+        else:
+            ip_address = select_item('Choose device for connection:', seen_addrs)
+
+    print('Connecting to device {}...'.format(ip_address))
+    vacuum = miio.Vacuum(ip=ip_address, token=known_token)
+    if not known_token:
+        print('Sending handshake to get token')
+        m = vacuum.do_discover()
+        vacuum.token = m.checksum
+
+    try:
+        s = vacuum.status()
+        if s.state == 'Updating':
+            print('Device already updating.')
+            exit()
+        elif s.state != 'Charging':
+            print('Put device to charging station for updating firmware.')
+            exit()
+    except Exception as ex:
+        print('error while checking device:', ex)
+        exit()
+
+    local_ip = findIP()
+
+    request_handler = http.server.SimpleHTTPRequestHandler
+    httpd = ThreadedHTTPServer(('', 0), request_handler)
+    http_port = httpd.server_address[1]
+
+    print('Starting local http server...')
+    thread = threading.Thread(target=httpd.handle_request, daemon=True)
+    thread.start()
+    print('Serving http server at {}:{}'.format(local_ip, http_port))
+
+    ota_params = {
+        'mode': 'normal',
+        'install': '1',
+        'app_url': 'http://{ip}:{port}/{fw}'.format(ip=local_ip, port=http_port, fw=firmware),
+        'file_md5': md5(firmware),
+        'proc': 'dnld install'
+    }
+    print('Sending ota command with parameters:', json.dumps(ota_params))
+    r = vacuum.send('miIO.ota', ota_params)
+    if r[0] == 'ok':
+        print('Ota started!')
+    else:
+        print('Got error response for ota command:', r)
+        exit()
+
+    ota_progress = 0
+    while ota_progress < 100:
+        ota_state = vacuum.send('miIO.get_ota_state')[0]
+        ota_progress = vacuum.send('miIO.get_ota_progress')[0]
+        printProgressBar(ota_progress, 100, prefix = 'Progress:', length = 50)
+        sleep(2)
+    print('Firmware downloaded successfully.')
+
+    httpd.server_close()
+
+    print('Exiting.')
     exit()
 
 
-
-
-cmd = "mirobo --ip="+ip+" --token=" + token + " status"
-print("Execute following for status:")
-print(cmd)
-#ret = os.system(cmd)
-#if ret != "status":
-#    print("Something gone wrong while getting status....")
-#    exit()
-
-
-localIP = findIP()
-updatefile = "v11_003077.pkg"
-datajson = {
-"mode":"normal",
-"install":"1",
-"app_url":"http://"+localIP+"/"+updatefile,
-"file_md5":md5(updatefile),
-"proc":"dnld install"
-}
-
-
-
-
-cmd = "mirobo --ip="+ip+" --token=" + token
-cmd = cmd +  " raw_command miIO.ota "
-cmd = cmd + "'" + json.dumps(datajson) + "'"
-print("Use following Command to update:")
-print(cmd)
-cmd = "python -m SimpleHTTPServer 80"
-print("Use following Command to start a Webserver: ")
-print(cmd)
-
+if __name__ == '__main__':
+    main()
