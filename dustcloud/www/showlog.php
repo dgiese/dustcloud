c91261f7765de6c2e96d639c7da40c05c5f1ae3b
Jens Schmer <jens.schmer+git@gmail.com>
2018-02-17 21:17:10 +0100

    Pretty print json data in recv msg log
    Former-commit-id: 64d24aa968009ae7f13ef315de9acf20e99de033

--- dustcloud/www/showlog.php
+++ dustcloud/www/showlog.php
@@ -12,6 +12,64 @@
 #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #GNU General Public License for more details.
 
+# taken from https://stackoverflow.com/a/9776726
+function prettyPrint( $json )
+{
+    $result = '';
+    $level = 0;
+    $in_quotes = false;
+    $in_escape = false;
+    $ends_line_level = NULL;
+    $json_length = strlen( $json );
+
+    for( $i = 0; $i < $json_length; $i++ ) {
+        $char = $json[$i];
+        $new_line_level = NULL;
+        $post = "";
+        if( $ends_line_level !== NULL ) {
+            $new_line_level = $ends_line_level;
+            $ends_line_level = NULL;
+        }
+        if ( $in_escape ) {
+            $in_escape = false;
+        } else if( $char === '"' || $char === "'") {
+            $in_quotes = !$in_quotes;
+        } else if( ! $in_quotes ) {
+            switch( $char ) {
+                case '}': case ']':
+                    $level--;
+                    $ends_line_level = NULL;
+                    $new_line_level = $level;
+                    break;
+
+                case '{': case '[':
+                    $level++;
+                case ',':
+                    $ends_line_level = $level;
+                    break;
+
+                case ':':
+                    $post = " ";
+                    break;
+
+                case " ": case "\t": case "\n": case "\r":
+                    $char = "";
+                    $ends_line_level = $new_line_level;
+                    $new_line_level = NULL;
+                    break;
+            }
+        } else if ( $char === '\\' ) {
+            $in_escape = true;
+        }
+        if( $new_line_level !== NULL ) {
+            $result .= "\n".str_repeat( "  ", $new_line_level );
+        }
+        $result .= $char.$post;
+    }
+
+    return $result;
+}
+
 $longlog = isset($_GET['longlog']) ? $_GET['longlog'] : '0';
 if ($longlog == 0)
 {
@@ -60,8 +118,15 @@ while ($row = $res->fetch_assoc())
 {
 		foreach ($row as $key => $value)
 	{
-		echo "$key : $value ";
-		echo "<br>";
+	    if ($key == "data")
+	    {
+	        $value = prettyPrint( $value );
+	        echo "$key : <pre>$value</pre>";
+	    }else
+	    {
+		    echo "$key : $value ";
+		    echo "<br>";
+	    }
 	}
 	echo "<hr>";
 }
