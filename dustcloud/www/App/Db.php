d097a618093af528063230d18fdf389f01c33e68
Thomas Tsiakalakis <tt@dplusc.de>
2018-07-26 19:55:01 +0200

    improved config error handling

--- dustcloud/www/App/Db.php
+++ dustcloud/www/App/Db.php
@@ -2,7 +2,7 @@
 
 namespace App;
 
- class DB {
+class DB {
     /**
      * @var \MySQLi
      */
--- dustcloud/www/bootstrap.php
+++ dustcloud/www/bootstrap.php
@@ -3,13 +3,23 @@
 require __DIR__ . '/vendor/autoload.php';
 
 define('APP_ROOT', __DIR__ . DIRECTORY_SEPARATOR);
-$config = parse_ini_file(__DIR__ . '/../config.ini', true);
 
+$configfile = __DIR__ . '/../config.ini';
+if(!file_exists($configfile) || !is_readable($configfile)){
+    die('could not open config.ini');
+}
 
-if($config['web']['debug']){
+$config = parse_ini_file($configfile, true);
+
+if(!array_key_exists('web', $config) || !array_key_exists('mysql', $config)){
+    die('config.ini is invalid.');
+}
+
+if(array_key_exists('debug', $config['web']) && $config['web']['debug']){
     error_reporting(E_ALL);
     ini_set('display_errors', 1);   
 }
 
 App\App::setConfig($config);
 unset($config);
+unset($configfile);
