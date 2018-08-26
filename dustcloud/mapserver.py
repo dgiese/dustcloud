0f95333cb4c3474467a2be0168825e6488ccf785
Thomas Tsiakalakis <tt@dplusc.de>
2018-08-26 14:00:43 +0200

    added more URLs to mapserver

--- dustcloud/mapserver.py
+++ dustcloud/mapserver.py
@@ -1,17 +1,15 @@
-#!/usr/bin/env python3
+# Author: Thomas Tsiakalakis [mail@tsia.de]
+# Copyright 2018 by Thomas Tsiakalakis
 
-# Author: Dennis Giese [dustcloud@1338-1.org]
-# Copyright 2017 by Dennis Giese
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
 
 import os
 import re
@@ -30,6 +28,7 @@ slamdata = {}
 class MyTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         data = self.request.recv(31).strip()
+        httplength = data.find(b'\n')
         if data[0:14] == b"ROCKROBO_MAP__":
             print("===== {} =====".format(self.client_address[0]))
             payload = self.request.recv(int(data[15:31]) + 1)
@@ -39,21 +38,34 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
             print(slam)
             if slam[1] == "estimate":
                 if not did in slamdata:
-                    slamdata[did] = {}
-                slamdata[did][round(float(slam[0]))] = {'t': float(slam[0]), 'x': float(slam[2]), 'y': float(slam[3]), 'z': float(slam[4])}
+                    slamdata[did] = []
+                if not did + "_prev" in slamdata:
+                    slamdata[did + "_prev"] = []
+                slamdata[did].append({'t': float(slam[0]), 'x': float(slam[2]), 'y': float(slam[3]), 'z': float(slam[4])})
             elif slam[1] == "reset":
                 if did in slamdata:
+                    if len(slamdata[did]) > 3:
+                        slamdata[did + "_prev"] = slamdata[did]
                     slamdata[did] = []
-        elif re.search(b'GET /([^ ]+) HTTP/1.?', data[0:22]):
+        elif re.search(b'GET /([^ /]+)(/all|/prev)? HTTP/1\\.[0-9]', data[0:httplength]):
             print("===== {} =====".format(self.client_address[0]))
-            print(data[0:22].decode())
-            did = re.search(b'GET /([^ ]+) HTTP/1.?', data[0:22]).group(1)
+            print(data[0:httplength].decode())
+            matches = re.search(b'GET /([^ /]+)(/all|/prev)? HTTP/1\\.[0-9]', data[0:httplength])
+            did = matches.group(1)
             print("HTTP Request for " + did.decode())
+            printall = matches.group(2)
             if did.decode() in slamdata:
                 print("OK")
                 http = "HTTP/1.0 200 OK\n"
                 http = http + "Content-Type: application/json; charset=UTF-8\n\n"
-                http = http + json.dumps(slamdata[did.decode()])
+                if printall == b"/all":
+                    print("all")
+                    http = http + json.dumps(slamdata[did.decode()])
+                elif printall == b"/prev":
+                    print("prev")
+                    http = http + json.dumps(slamdata[did.decode() + "_prev"])
+                else:
+                    http = http + json.dumps(slamdata[did.decode()][-3:])
             else:
                 print("Not Found")
                 http = "HTTP/1.0 404 Not Found\n"
@@ -83,4 +95,3 @@ class TCPSimpleServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
 if __name__ == "__main__":
 	server = TCPSimpleServer((listenaddr, listenport), MyTCPHandler)
 	server.serve_forever()
-
