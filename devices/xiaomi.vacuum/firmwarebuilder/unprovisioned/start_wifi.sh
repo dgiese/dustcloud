4576a7a6d20e2176c88a641ab7331dacb2f8ce71
jansohn <git@jansohn.net>
2018-12-29 15:02:09 +0100

    refactored start_wifi.sh to work on gen2 robots

--- devices/xiaomi.vacuum/firmwarebuilder/unprovisioned/start_wifi.sh
+++ devices/xiaomi.vacuum/firmwarebuilder/unprovisioned/start_wifi.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-file="/opt/unprovisioned/wpa_supplicant.conf"
-if [ ! -f "$file" ]
+config="/opt/unprovisioned/wpa_supplicant.conf"
+log="/opt/unprovisioned/wpa_supplicant.log"
+
+if [ ! -r "$config" ]
 then
-    echo "$0: File '${file}' not found."
+    echo "$0: File '${config}' not found." > "$log"
 else
     #add enough time to fix wrong wireless settings
     sleep 200
 
     #disable accesspoint
-    ifdown wlan0 > /dev/null 2>&1
-    ifconfig wlan0 down > /dev/null 2>&1
-    killall hostapd >/dev/null 2>&1
-    iw mon.wlan0 del >/dev/null 2>&1
-    create_ap --stop wlan0 > /dev/null 2>&1
-    killall wpa_supplicant >/dev/null 2>&1
-    killall dhclient >/dev/null 2>&1
+    echo "Stopping AP..." > "$log"
+    create_ap --stop wlan0 >> "$log" 2>&1
+    echo "Killing leftover AP daemons..." >> "$log"
+    killall hostapd >> "$log" 2>&1
 
     #login to your network
-    ifconfig wlan0 up >/dev/null 2>&1
-    /sbin/wpa_supplicant -s -B -P /var/run/wpa_supplicant_1.wlan0.pid -i wlan0 -D nl80211,wext -c /mnt/data/unprovisioned/wpa_supplicant.conf -C /var/run/wpa_supplicant >/dev/null 2>&1
-    dhclient wlan0 >/dev/null 2>&1
+    echo "Starting WPA supplicant background process..." >> "$log"
+    /sbin/wpa_supplicant -B -iwlan0 -Dnl80211 -c"$config" >> "$log" 2>&1
+    echo "Requesting IP and network settings from DHCP server..." >> "$log"
+    dhclient wlan0 >> "$log" 2>&1
 fi
