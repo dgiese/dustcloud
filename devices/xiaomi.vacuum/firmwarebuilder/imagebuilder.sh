bf9c082e2e2afcfe6b1304adf68cf9848c673e1e
Andreas Schneider <asn@cryptomilk.org>
2019-01-21 10:45:54 +0100

    Fix using relative path with --rrlogd-patcher=../patcher.py

--- devices/xiaomi.vacuum/firmwarebuilder/imagebuilder.sh
+++ devices/xiaomi.vacuum/firmwarebuilder/imagebuilder.sh
@@ -429,27 +429,33 @@ if [ $DISABLE_LOGS -eq 1 ]; then
 fi
 
 if [ $PATCH_RRLOGD -eq 1 ]; then
-    PYTHON=${PYTHON:-"python"}
-    echo "Creating backup of rrlogd"
-    cp $IMG_DIR/opt/rockrobo/rrlog/rrlogd $IMG_DIR/opt/rockrobo/rrlog/rrlogd.xiaomi
+    PYTHON=${PYTHON:-"python3"}
+    RRLOGD_PATCHER_ABS="$(readlink_f $RRLOGD_PATCHER 2> /dev/null)"
 
-    # This is a extremly simple binary patch by John Rev
-    # In the long run we should use his rrlogd-patcher however we would need to integrate
-    # it into the imagebuilder package or git repo.
-    #
-    # See https://github.com/JohnRev/rrlogd-patcher
-    echo "Trying to patch rrlogd"
-    cp $IMG_DIR/opt/rockrobo/rrlog/rrlogd $FW_TMPDIR/rrlogd
+    if [ -r "$RRLOGD_PATCHER_ABS" ]; then
+        echo "Creating backup of rrlogd"
+        cp $IMG_DIR/opt/rockrobo/rrlog/rrlogd $IMG_DIR/opt/rockrobo/rrlog/rrlogd.xiaomi
 
-    pushd $FW_TMPDIR
-    $PYTHON $RRLOGD_PATCHER
-    ret=$?
-    popd
-    if [ $ret -eq 0 ]; then
-        install -m 0755 $FW_TMPDIR/rrlogd_patch $IMG_DIR/opt/rockrobo/rrlog/rrlogd
-        echo "Successfully patched rrlogd"
+        # This is a extremly simple binary patch by John Rev
+        # In the long run we should use his rrlogd-patcher however we would need to integrate
+        # it into the imagebuilder package or git repo.
+        #
+        # See https://github.com/JohnRev/rrlogd-patcher
+        echo "Trying to patch rrlogd"
+        cp $IMG_DIR/opt/rockrobo/rrlog/rrlogd $FW_TMPDIR/rrlogd
+
+        pushd $FW_TMPDIR
+        $PYTHON "$RRLOGD_PATCHER_ABS"
+        ret=$?
+        popd
+        if [ $ret -eq 0 ]; then
+            install -m 0755 $FW_TMPDIR/rrlogd_patch $IMG_DIR/opt/rockrobo/rrlog/rrlogd
+            echo "Successfully patched rrlogd"
+        else
+            echo "Failed to patch rrlogd (please report a bug here: https://github.com/JohnRev/rrlogd-patcher/issues)"
+        fi
     else
-        echo "Failed to patch rrlogd (please report a bug here: https://github.com/JohnRev/rrlogd-patcher/issues)"
+        echo "Invalid path to rrlogd ($RRLOGD_PATCHER_ABS)"
     fi
 fi
 
