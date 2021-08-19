95673c4063155c0c9ba5dcb59274a47aae7c2716
Tuckie <Tuckie@users.noreply.github.com>
2021-08-19 12:38:45 -0400

    Update install-manual.sh
    Grammar tweaks

--- tools/firmwarebuilder-ng/features/fwinstaller_1c/install-manual.sh
+++ tools/firmwarebuilder-ng/features/fwinstaller_1c/install-manual.sh
@@ -25,11 +25,11 @@ maximumsize=26000000
 minimumsize=20000000
 actualsize=$(wc -c < /tmp/rootfs.img)
 if [ "$actualsize" -ge "$maximumsize" ]; then
-	echo "(!!!) rootfs.img looks to big. The size might exceed the available space on the flash. Aborting the installation"
+	echo "(!!!) rootfs.img looks too big. The size might exceed the available space on the flash. Aborting the installation"
 	exit 1
 fi
 if [ "$actualsize" -le "$minimumsize" ]; then
-	echo "(!!!) rootfs.img looks to small. Maybe something went wrong with the image generation. Aborting the installation"
+	echo "(!!!) rootfs.img looks too small. Maybe something went wrong with the image generation. Aborting the installation"
 	exit 1
 fi
 
@@ -45,7 +45,7 @@ if [[ -f /tmp/boot.img ]]; then
 		#set -x
 		source /usr/bin/config
 
-		echo "Start installation ..."
+		echo "Starting installation ..."
 
 		BOOT_MODE=$(fw_printenv boot_partition)
 
@@ -53,13 +53,13 @@ if [[ -f /tmp/boot.img ]]; then
 
 		if [ $? -eq 0 ]
 		then
-				echo "We are currently on rootfs1, will installing on rootfs2"
+				echo "We are currently on rootfs1, will install on rootfs2"
 				BOOT_PART=/dev/by-name/boot2
 				ROOT_FS_PART=/dev/by-name/rootfs2
 				BOOT_PARTITION=boot2
 				ROOT_PARTITION=rootfs2
 		else
-				echo "We are currently on rootfs2, will installing on rootfs1"
+				echo "We are currently on rootfs2, will install on rootfs1"
 				BOOT_PART=/dev/by-name/boot1
 				ROOT_FS_PART=/dev/by-name/rootfs1
 				BOOT_PARTITION=boot1
@@ -91,7 +91,7 @@ if [[ -f /tmp/boot.img ]]; then
 						reboot -n -f
 				else
 				  sync
-				  echo "(!!!) Did not found marker in updated firmware. Update likely failed, wont change the boot order"
+				  echo "(!!!) Did not find marker in updated firmware. Update likely failed, wont change the boot order"
 				  exit 1
 				fi
 		else
